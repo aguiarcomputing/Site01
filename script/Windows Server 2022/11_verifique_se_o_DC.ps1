6a081e74ef841d072eadd3fe6e4b8188711943ce
Aguiar <leandro.aguiar@outlook.com>
2025-04-12 11:24:41 -0300

    Create 11_verifique_se_o_DC.ps1

---
+++ script/Windows Server 2022/11_verifique_se_o_DC.ps1
@@ -0,0 +1,9 @@
+﻿# Verificar status do AD DS
+Get-ADDomain | Select-Object Name, DomainMode
+Get-ADForest | Select-Object Name, ForestMode
+
+# Verificar registros DNS
+nslookup $DomainName
+
+# Testar integridade do DC
+dcdiag /v
