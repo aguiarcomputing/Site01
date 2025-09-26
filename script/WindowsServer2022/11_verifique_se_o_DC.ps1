ba88002d5e934817adc8c2013d6bccbcc61b610b
Aguiar <127853365+aguiarcomputing@users.noreply.github.com>
2025-09-26 16:54:05 -0300

    as

--- script/WindowsServer2022/11_verifique_se_o_DC.ps1
+++
@@ -1,9 +0,0 @@
-﻿# Verificar status do AD DS
-Get-ADDomain | Select-Object Name, DomainMode
-Get-ADForest | Select-Object Name, ForestMode
-
-# Verificar registros DNS
-nslookup $DomainName
-
-# Testar integridade do DC
-dcdiag /v
