c09365281514741c6a8400a9ee54bb15c447124c
LEANDRO AGUIAR <127853365+aguiarcomputing@users.noreply.github.com>
2025-12-11 10:17:37 -0300

    sa

--- MobaXterm/slash/mx86_64b/etc/sessionsaliases.sh
+++ MobaXterm/slash/mx86_64b/etc/sessionsaliases.sh
@@ -1,6 +1,9 @@
 function _192_168_2_101() {
 "/home/mobaxterm/LauncherFolder/MobaXterm_Personal_25.3.exe" -bookmark2 "#91#4%192.168.2.101%3389%%0%0%0%0%-1%0%0%-1%%%%%0%0%%-1%%-1%-1%0%-1%0%-1%0%0%0%0%#MobaFont%10%0%0%-1%15%236,236,236%30,30,30%180,180,192%0%-1%0%%xterm%-1%0%_Std_Colors_0_%80%24%0%1%-1%<none>%%0%0%-1%0%#0# #-1"
 }
+function _138_122_64_25433389() {
+"/home/mobaxterm/LauncherFolder/MobaXterm_Personal_25.3.exe" -bookmark2 " #91#4%138.122.64.254%33389%%0%0%0%0%-1%0%0%-1%%%%%0%0%%-1%%-1%-1%0%-1%0%-1%0%0%0%0%#MobaFont%10%0%0%-1%15%236,236,236%30,30,30%180,180,192%0%-1%0%%xterm%-1%0%_Std_Colors_0_%80%24%0%1%-1%<none>%%0%0%-1%0%#0# #-1"
+}
 function srv98_prodns_com_br2083logoutlocale() {
 "/home/mobaxterm/LauncherFolder/MobaXterm_Personal_25.3.exe" -bookmark2 "pt_br= #313#11%https://srv98.prodns.com.br:2083/logout/?locale__EQUAL__pt_br%-1%-1%-1%-1%-1%-1%-1%0%0%3%-1%-1%0%-1%0%-1%0%%#MobaFont%10%0%0%-1%15%236,236,236%30,30,30%180,180,192%0%-1%0%%xterm%-1%0%_Std_Colors_0_%80%24%0%1%-1%<none>%%0%0%-1%0%#0# #-1"
 }
