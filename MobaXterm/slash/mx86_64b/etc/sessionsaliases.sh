e308db40ff1ab698b7c3e1e812282339df0f27dd
LEANDRO AGUIAR <127853365+aguiarcomputing@users.noreply.github.com>
2025-12-15 17:35:15 -0300

    sa

--- MobaXterm/slash/mx86_64b/etc/sessionsaliases.sh
+++ MobaXterm/slash/mx86_64b/etc/sessionsaliases.sh
@@ -2,7 +2,7 @@ function _192_168_2_101() {
 "/home/mobaxterm/LauncherFolder/MobaXterm_Personal_25.3.exe" -bookmark2 "#91#4%192.168.2.101%3389%%0%0%0%0%-1%0%0%-1%%%%%0%0%%-1%%-1%-1%0%-1%0%-1%0%0%0%0%#MobaFont%10%0%0%-1%15%236,236,236%30,30,30%180,180,192%0%-1%0%%xterm%-1%0%_Std_Colors_0_%80%24%0%1%-1%<none>%%0%0%-1%0%#0# #-1"
 }
 function _138_122_64_25433389() {
-"/home/mobaxterm/LauncherFolder/MobaXterm_Personal_25.3.exe" -bookmark2 " #91#4%138.122.64.254%33389%%0%0%0%0%-1%0%0%-1%%%%%0%0%%-1%%-1%-1%0%-1%0%-1%0%0%0%0%#MobaFont%10%0%0%-1%15%236,236,236%30,30,30%180,180,192%0%-1%0%%xterm%-1%0%_Std_Colors_0_%80%24%0%1%-1%<none>%%0%0%-1%0%#0# #-1"
+"/home/mobaxterm/LauncherFolder/MobaXterm_Personal_25.3.exe" -bookmark2 "#91#4%138.122.64.254%33389%%0%0%0%0%-1%0%0%-1%%%%%0%0%%-1%%-1%-1%0%-1%0%-1%0%0%0%0%#MobaFont%10%0%0%-1%15%236,236,236%30,30,30%180,180,192%0%-1%0%%xterm%-1%0%_Std_Colors_0_%80%24%0%1%-1%<none>%%0%0%-1%0%#0# #-1"
 }
 function srv98_prodns_com_br2083logoutlocale() {
 "/home/mobaxterm/LauncherFolder/MobaXterm_Personal_25.3.exe" -bookmark2 "pt_br= #313#11%https://srv98.prodns.com.br:2083/logout/?locale__EQUAL__pt_br%-1%-1%-1%-1%-1%-1%-1%0%0%3%-1%-1%0%-1%0%-1%0%%#MobaFont%10%0%0%-1%15%236,236,236%30,30,30%180,180,192%0%-1%0%%xterm%-1%0%_Std_Colors_0_%80%24%0%1%-1%<none>%%0%0%-1%0%#0# #-1"
@@ -15,6 +15,12 @@ function _192_168_6_249_leandro_INTUS_() {
 }
 function _192_168_150_124() {
 "/home/mobaxterm/LauncherFolder/MobaXterm_Personal_25.3.exe" -bookmark2 "#109#0%192.168.150.124%22%%%-1%-1%%%%%0%-1%0%%%-1%-1%0%0%%1080%%0%0%1%%0%%%%0%-1%-1%0%%#MobaFont%10%0%0%-1%15%236,236,236%30,30,30%180,180,192%0%-1%0%%xterm%-1%0%_Std_Colors_0_%80%24%0%1%-1%<none>%%0%0%-1%0%#0# #-1"
+}
+function _192_168_150_226() {
+"/home/mobaxterm/LauncherFolder/MobaXterm_Personal_25.3.exe" -bookmark2 " #109#0%192.168.150.226%22%%%-1%-1%%%%%0%-1%0%%%-1%-1%0%0%%1080%%0%0%1%%0%%%%0%-1%-1%0%%#MobaFont%10%0%0%-1%15%236,236,236%30,30,30%180,180,192%0%-1%0%%xterm%-1%0%_Std_Colors_0_%80%24%0%1%-1%<none>%%0%0%-1%0%#0# #-1"
+}
+function _192_168_150_226_1_() {
+"/home/mobaxterm/LauncherFolder/MobaXterm_Personal_25.3.exe" -bookmark2 " #109#0%192.168.150.226%22%%%-1%-1%%%%%0%-1%0%%%-1%-1%0%0%%1080%%0%0%1%%0%%%%0%-1%-1%0%%#MobaFont%10%0%0%-1%15%236,236,236%30,30,30%180,180,192%0%-1%0%%xterm%-1%0%_Std_Colors_0_%80%24%0%1%-1%<none>%%0%0%-1%0%#0# #-1"
 }
 function ITC-PVE01-S2_-_Proxmox_Virtual_Environment() {
 "/home/mobaxterm/LauncherFolder/MobaXterm_Personal_25.3.exe" -bookmark2 "#147#11%https://172.16.2.11:8006/__DIEZE__v1:0:18:4:::::::%-1%-1%-1%-1%-1%-1%-1%0%0%1%-1%-1%0%-1%0%-1%0%%#MobaFont%10%0%0%-1%15%236,236,236%30,30,30%180,180,192%0%-1%0%%xterm%-1%0%_Std_Colors_0_%80%24%0%1%-1%<none>%%0%0%-1%0%#0# #-1"
